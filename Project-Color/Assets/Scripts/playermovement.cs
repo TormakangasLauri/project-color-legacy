f0e2c2a9f2547fa497d94570a53ad06dc527ed71
Seppänen Miko <miko.seppanen06@gmail.com>
2024-03-24 19:28:56 +0200

    Wallrunning and walljumping working
    Moving in wallrun changed from AddFroce() to directing player velocity

--- Project-Color/Assets/Scripts/playermovement.cs
+++ Project-Color/Assets/Scripts/playermovement.cs
@@ -13,17 +13,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public InputActionReference move;
     
-    public Rigidbody rb;
+    private Rigidbody rb;
     new public Camera camera;
 
     private Vector2 moveDirection; //moveDirecton
     [Header("Values")]
-    public float speed;
-    public float maxSpeed = 50;
-    public float jumpForce;
-    public float gravity;
+    public float speed = 100;
+    public float maxSpeed = 10;
+    public float jumpForce = 12;
+    public float gravity = 20;
     public float landingGracePeriod = 0.2f;
     private float landingGrace;
+    private Vector3 wallRunForce;
     
     [Header("Camera")]
     public float mouseSensitivity = 2f;
@@ -37,20 +38,21 @@ public class PlayerMovement : MonoBehaviour
     public GameObject wallCheck;
     public LayerMask wallLayer;
     public bool walled;
-    public List<Collider> wallColList = new List<Collider>();
+    [HideInInspector] public List<Collider> wallColList = new List<Collider>();
     private Vector3 dirToWall;
 
-    [Header("Bools")] 
+    [Header("Bools")]
     public bool wallRiding;
+    private bool firstWallRideCall = true;
     public bool pressingJump;
     private bool hasJumped;
     private float timeSinceJump;
-
 
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        rb = gameObject.GetComponent<Rigidbody>();
     }
     
     void Update()
@@ -78,25 +80,19 @@ public class PlayerMovement : MonoBehaviour
 
     void WallRide()
     {
-        Vector2 dirToWall90 = Quaternion.Euler(0, 90, 0) * dirToWall;
-        Vector3 movement = new Vector3(moveDirection.x * speed, 0, moveDirection.y * speed);
-        Vector3 worldMovement = transform.rotation * movement;
-        Vector2 direction2 = new Vector2(worldMovement.x, worldMovement.z);
-        float angle2 = Mathf.Rad2Deg * Mathf.Acos((worldMovement.x * dirToWall90.x + worldMovement.z * dirToWall90.y) / (dirToWall90.magnitude * direction2.magnitude));
         Vector2 dirToWall2 = new Vector2(dirToWall.x, dirToWall.z);
-        float angle = Mathf.Rad2Deg * Mathf.Acos((worldMovement.x * dirToWall.x + worldMovement.z * dirToWall.z) / (dirToWall2.magnitude * direction2.magnitude)); // K uopion Y liopistollinen S airaala
+        Vector3 dirToWall90 = Quaternion.Euler(0, 90, 0) * dirToWall;
 
+        Vector3 forward = transform.rotation * Vector3.forward;
+        Vector3 forward90 = Quaternion.Euler(0, 90, 0) * forward;
+        
+        float camAngle90 = Mathf.Rad2Deg * Mathf.Acos((forward90.x * dirToWall.x + forward90.z * dirToWall.z) / (dirToWall2.magnitude * forward90.magnitude));
+        
+        if (firstWallRideCall) wallRunForce = dirToWall90.normalized * (Mathf.Sign(camAngle90 - 90) * speed);
+        firstWallRideCall = false;
+        
         // Apply force to ride the wall
-        rb.AddForce(Quaternion.Euler(0, Mathf.Sign(angle) * 90, 0) * dirToWall.normalized * (movement.magnitude * Mathf.Sign(-angle2 + 90)));
-
-        // Set y-component of velocity to zero to prevent downward movement
-        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-
-        // Normal speed limit
-        if (rb.velocity.magnitude > maxSpeed)
-        {
-            rb.AddRelativeForce(-(Quaternion.Euler(0, Mathf.Sign(angle) * 90, 0) * dirToWall.normalized * (movement.magnitude * Mathf.Sign(-angle2 + 90))));
-        }
+        rb.velocity = (wallRunForce / 10);
     }
 
     
@@ -113,9 +109,6 @@ public class PlayerMovement : MonoBehaviour
         // Angle between the direction of where the player is going and the wall
         float angle = Mathf.Rad2Deg * Mathf.Acos((worldMovement.x * dirToWall.x + worldMovement.z * dirToWall.z) / (dirToWall2.magnitude * direction2.magnitude));
 
-        Vector2 dirToWall90 = Quaternion.Euler(0, 90, 0) * dirToWall;
-     
-
         float scale = (angle / 90f);
         float scale2 = -scale + 1; 
         
@@ -125,7 +118,7 @@ public class PlayerMovement : MonoBehaviour
             // Normal speed limit
             if (velocity.magnitude > maxSpeed) rb.AddRelativeForce(-movement);
             // Slows the player when there are no movement inputs
-            if (movement == Vector3.zero && velocity.magnitude > 2) rb.velocity = new Vector3(velocity.x * 0.7f, velocity.y, velocity.z * 0.7f);
+            if (movement == Vector3.zero && velocity.magnitude > 2) rb.velocity = new Vector3(velocity.x * 0.6f, velocity.y, velocity.z * 0.6f);
         }
         else
         {
@@ -138,7 +131,6 @@ public class PlayerMovement : MonoBehaviour
 
         if (!grounded && pressingJump && movement.z > 0 && walled)
             wallRiding = true;
-        Debug.Log(new Vector2(velocity.x, velocity.z).magnitude);
     }
     
     
@@ -155,6 +147,7 @@ public class PlayerMovement : MonoBehaviour
         if (action.canceled)
         {
             pressingJump = false;
+            if (wallRiding) WallJump();
             wallRiding = false;
         }
     }
@@ -163,12 +156,12 @@ public class PlayerMovement : MonoBehaviour
         Vector3 velocity = rb.velocity;
         Quaternion localRotation = transform.localRotation;
         // Calculates the x and z components of the velocity added to the player when jumping
-        float velX = moveDirection.x * velocity.magnitude * Mathf.Cos(localRotation.eulerAngles.y * Mathf.Deg2Rad) + moveDirection.y * velocity.magnitude * Mathf.Sin(localRotation.eulerAngles.y * Mathf.Deg2Rad);
-        float velZ = -moveDirection.x * velocity.magnitude * Mathf.Sin(localRotation.eulerAngles.y * Mathf.Deg2Rad) + moveDirection.y * velocity.magnitude * Mathf.Cos(localRotation.eulerAngles.y * Mathf.Deg2Rad);
+        float moveX = moveDirection.x * velocity.magnitude * Mathf.Cos(localRotation.eulerAngles.y * Mathf.Deg2Rad) + moveDirection.y * velocity.magnitude * Mathf.Sin(localRotation.eulerAngles.y * Mathf.Deg2Rad);
+        float moveZ = -moveDirection.x * velocity.magnitude * Mathf.Sin(localRotation.eulerAngles.y * Mathf.Deg2Rad) + moveDirection.y * velocity.magnitude * Mathf.Cos(localRotation.eulerAngles.y * Mathf.Deg2Rad);
 
         Vector2 velocity2 = new Vector2(velocity.x, velocity.z);
-        Vector2 direction2 = new Vector2(velX, velZ);
-        float velAngle = Mathf.Rad2Deg * Mathf.Acos((velocity.x * velX + velocity.z * velZ) / (velocity2.magnitude * direction2.magnitude));
+        Vector2 direction2 = new Vector2(moveX, moveZ);
+        float velAngle = Mathf.Rad2Deg * Mathf.Acos((velocity.x * moveX + velocity.z * moveZ) / (velocity2.magnitude * direction2.magnitude));
         float scale = -((velAngle / 180f) * (1f + 0.2f) - 0.2f) + 0.8f;
 
         if (landingGrace > Time.realtimeSinceStartup && !hasJumped && grounded) //Jump if player has landed within the grace period and has not yet jumped
@@ -176,11 +169,22 @@ public class PlayerMovement : MonoBehaviour
             if (moveDirection.x == 0 && moveDirection.y == 0)
                 rb.velocity = new Vector3(velocity.x, jumpForce, velocity.z);
             else
-                rb.velocity = new Vector3(velX * scale, jumpForce, velZ * scale);
+                rb.velocity = new Vector3(moveX * scale, jumpForce, moveZ * scale);
             hasJumped = true;
             timeSinceJump = Time.time;
         }
         if (timeSinceJump + 0.2 < Time.time) hasJumped = false;
+    }
+
+    void WallJump()
+    {
+        Vector3 velocity = rb.velocity;
+        
+        // Walljumping throws the player away from the wall
+        rb.velocity = new Vector3(velocity.x, jumpForce, velocity.z) - dirToWall.normalized * 5;
+        
+        hasJumped = true;
+        firstWallRideCall = true;
     }
     
     void CameraRotation()
