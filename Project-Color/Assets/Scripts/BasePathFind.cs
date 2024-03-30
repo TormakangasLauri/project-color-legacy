9b9da6aeae69871abedb7a10487da7196508713f
TheFinguy <vilho.luosma@gmail.com>
2024-03-30 21:53:08 +0200

    Improved pathfinding
    Enemies now only path towards the player if they have seen it in the last second. Otherwise they path to the last known position or to themselves if the player has not yet been seen.
    
    Co-Authored-By: Miko <115464052+mkeyos@users.noreply.github.com>

--- Project-Color/Assets/Scripts/BasePathFind.cs
+++ Project-Color/Assets/Scripts/BasePathFind.cs
@@ -3,14 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class BasePathFind : MonoBehaviour
 {
     public NavMeshAgent vihu;
     public GameObject player;
-
+    [FormerlySerializedAs("blockloslayer")] public LayerMask noloslayer;
+    private float lostlostimer;
+    
+    
     private void Update()
     {
-        vihu.SetDestination(player.transform.position);
+        lostlostimer -= Time.deltaTime;
+        if (!Physics.Raycast(transform.position, (player.transform.position - transform.position).normalized, (player.transform.position - transform.position).magnitude, noloslayer))
+            lostlostimer = 1;
+        if (lostlostimer >= 0)
+            vihu.SetDestination(player.transform.position);
+        
     }
 }
