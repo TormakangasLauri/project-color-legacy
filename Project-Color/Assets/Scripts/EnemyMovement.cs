50cc6628efd165458fe77a5204a8e07ec5da1169
Seppänen Miko <miko.seppanen06@gmail.com>
2024-04-17 11:37:59 +0300

    Better enemy movement
    Enemy tilts weirldy, fix this

--- Project-Color/Assets/Scripts/EnemyMovement.cs
+++ Project-Color/Assets/Scripts/EnemyMovement.cs
@@ -1,31 +1,89 @@
 using System;
+using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Rigidbody rb;
+
     public GameObject target;
-    private Vector3 targetPos;
     public LayerMask terrainLayer;
-    public GameObject pathFind;
+
+    public float speed;
+    
+    public bool LOSToPlayer;
+
+    public enum State { idle, navmesh, los, attack };
+    public State state;
 
     private void Start()
     {
-        agent = GetComponentInChildren<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = true;
         rb = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("Player");
+
+        agent.speed = speed;
+        state = State.navmesh;
     }
 
-    void Update()
+    private void Update()
     {
-        pathFind.transform.position = transform.position;
+        LOSToPlayer = !Physics.Linecast(transform.position, target.transform.position, terrainLayer);
     }
 
     private void FixedUpdate()
     {
-        targetPos = GetComponentInChildren<PathFind>().targetPos;
-        rb.AddForce((targetPos - transform.position).normalized * 50);
-        if (rb.velocity.magnitude > 5) rb.AddForce((targetPos - transform.position).normalized * -50);
+        // States
+        //  idle: not moving / wandering, will finish later
+        //  navmesh: moving with NavMeshAgent when player is not in LOS
+        //  los: moving with force when player in LOS
+        switch (state)
+        {
+            case State.idle:
+                break;
+            case State.navmesh:
+                NavMeshMovement();
+                break;
+            case State.los:
+                LOSMovement();
+                break;
+        }
+    }
+
+    private void NavMeshMovement()
+    {
+        agent.SetDestination(target.transform.position);
+
+        // State change check
+        if (LOSToPlayer)
+        {
+            agent.enabled = false;
+            state = State.los;
+        }
+    }
+
+    private void LOSMovement()
+    {
+        Vector3 targetPos = target.transform.position;
+        Vector3 pos = transform.position;
+        Vector3 directionToPlayer = new Vector3(targetPos.x - pos.x, 0, targetPos.z - pos.z).normalized;
+        Vector3 movement = directionToPlayer * (speed * 10);
+        
+        rb.angularVelocity = Vector3.up * (-5 * Mathf.Deg2Rad * Vector3.SignedAngle(directionToPlayer, transform.forward, Vector3.up));
+        transform.rotation = Quaternion.Euler(0, transform.rotation.y, 0);
+
+        rb.AddForce(movement);
+        if (rb.velocity.magnitude > speed) rb.AddForce(-movement);
+        
+        // State change check
+        if (!LOSToPlayer)
+        {
+            agent.enabled = true;
+            state = State.navmesh;
+        }
     }
 }
