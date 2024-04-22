06285c614f73a7a4972d1511b405c110cc9ce8f1
Seppänen Miko <miko.seppanen06@gmail.com>
2024-04-22 23:01:13 +0300

    PaintSplash gets the accurate uv coordinates
    Only works if the other object has a meshcollider

--- Project-Color/Assets/Scripts/Painting/PaintSplash.cs
+++ Project-Color/Assets/Scripts/Painting/PaintSplash.cs
@@ -14,17 +14,34 @@ public class PaintSplash : MonoBehaviour
         collisionEvents = new List<ParticleCollisionEvent>();
     }
 
-    // void OnParticleCollision(GameObject other)  // Does not work.
-    // {
-    //     Paintable paintable = other.gameObject.GetComponent<Paintable>();
-    //
-    //     foreach (ParticleCollisionEvent collisionEvent in collisionEvents)
-    //     {
-    //         if (paintable)
-    //         {
-    //             paintable.AddPaint();
-    //         }
-    //     }
-    // }
+    void OnParticleCollision(GameObject other)
+    {
+        Paintable paintable = other.gameObject.GetComponent<Paintable>();
+        
+        // foreach (ParticleCollisionEvent collisionEvent in collisionEvents)
+        // {
+        //     if (paintable)
+        //     {
+        //         paintable.AddPaint();
+        //     }
+        // }
+
+        // Following code made by non other than mkeyos
+        // Only works if the other object has a meshcollider, otherwise colUV is always (0, 0)
+        
+        int colEvents = ps.GetCollisionEvents(other, collisionEvents);
+        for (int i = 0; i < colEvents; i++)
+        {
+            Vector3 colNormal = collisionEvents[i].normal;
+            RaycastHit hit;
+            Physics.Raycast(collisionEvents[i].intersection, -colNormal, out hit);
+            if (hit.collider.GetComponent<Renderer>() != null)
+            {
+                Vector2 colUV = hit.textureCoord;
+                Debug.Log(colUV);
+                // paintable.AddPaint(colUV, "texture", "color");
+            }
+        }
+    }
 }
 
