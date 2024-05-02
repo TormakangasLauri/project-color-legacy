e893731e8896aaab29beb68d295d3e729241ac42
Seppänen Miko <miko.seppanen06@gmail.com>
2024-05-02 18:54:48 +0300

    Improved EnemyMovement and added multiple navmeshes
    Different size enemies use different  navmeshes controlled by Level-object. Level also contains all terrain.

--- Project-Color/Assets/Scripts/Enemies/EnemyMovement.cs
+++ Project-Color/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -13,7 +13,7 @@ public class EnemyMovement : MonoBehaviour
     private EnemyType ET;
 
     private GameObject target;
-    
+
     [HideInInspector] public LayerMask terrainLayer;
     public NavMeshPath path;
 
@@ -21,8 +21,6 @@ public class EnemyMovement : MonoBehaviour
     [HideInInspector] public float stopDistance;
     public bool LOSToTarget;
     public bool grounded;
-    
-    private float stateSwitchTimer;
 
     public enum State { idle, navmesh, los, attack };
     public State state;
@@ -43,8 +41,6 @@ public class EnemyMovement : MonoBehaviour
         Grounded();
         LOSToTarget = !Physics.Linecast(transform.position, target.transform.position, terrainLayer);
 
-        stateSwitchTimer -= Time.deltaTime;
-
         path = ET.path;
     }
 
@@ -52,12 +48,12 @@ public class EnemyMovement : MonoBehaviour
     {
         // States
         //  idle: not moving / wandering, will finish later
-        //  navmesh: moving using a NavMeshPath when player is not in LOS
-        //  los: moving with force towards the player when player is in LOS
+        //  Move: move with force towards the next corner of NavMeshPath
         //  attack: attacking
         switch (state)
         {
             case State.idle:
+                Idle();
                 break;
             case State.navmesh:
                 NavMeshMovement();
@@ -65,10 +61,18 @@ public class EnemyMovement : MonoBehaviour
             case State.los:
                 LOSMovement();
                 break;
+            case State.attack:
+                Attack();
+                break;
         }
 
         // Gravity
         if (rb.velocity.y < 0) rb.AddForce(Vector3.down * 20);
+    }
+
+    private void Idle()
+    {
+
     }
 
     private void NavMeshMovement()
@@ -80,11 +84,10 @@ public class EnemyMovement : MonoBehaviour
             Vector3 pos = transform.position;
             Vector3 directionToTarget = new Vector3(cornerPos.x - pos.x, 0, cornerPos.z - pos.z).normalized;
             Vector3 movement = directionToTarget * (speed * 10);
+            float distOnXZ = Vector3.Distance(new Vector3(pos.x, 0, pos.z), new Vector3(targetPos.x, 0, targetPos.z));
 
             // Rotate to face the player
             rb.MoveRotation(Quaternion.LookRotation(directionToTarget));
-
-            float distOnXZ = Vector3.Distance(new Vector3(pos.x, 0, pos.z), new Vector3(targetPos.x, 0, targetPos.z));
 
             // Moving when not in stopping distance of the target
             if (distOnXZ > stopDistance + stopDistance / 2) rb.AddForce(movement);
@@ -92,6 +95,7 @@ public class EnemyMovement : MonoBehaviour
             else if (rb.velocity.magnitude > 0.5) rb.AddForce(-rb.velocity * 2);
             // Speed limit
             if (rb.velocity.magnitude > speed) rb.AddForce(-movement);
+            // Move away from the target when too close
             if (distOnXZ < 2)
             {
                 rb.AddForce(-movement / 3);
@@ -103,54 +107,61 @@ public class EnemyMovement : MonoBehaviour
         Physics.Raycast(target.transform.position, Vector3.down, out hit, 100, terrainLayer);
 
         // State change check
-        if (LOSToTarget && (path.corners.Length <= 2 || hit.point.y <= transform.position.y - transform.localScale.y / 2) && stateSwitchTimer < 0)
+        if (LOSToTarget && (path.corners.Length <= 2 || hit.point.y <= transform.position.y - transform.localScale.y / 2))
         {
-            stateSwitchTimer = 1;
-            state = State.los;
+            StartCoroutine(NavMeshToLOS());
         }
+    }
+    IEnumerator NavMeshToLOS()
+    {
+        yield return new WaitForSeconds(0.5f);
+        state = State.los;
     }
 
     private void LOSMovement()
     {
         Vector3 targetPos = target.transform.position;
         Vector3 pos = transform.position;
-        Vector3 directionToPlayer = new Vector3(targetPos.x - pos.x, 0, targetPos.z - pos.z).normalized;
-        Vector3 movement = directionToPlayer * (speed * 10);
-        
-        // Rotate to face the player
-        rb.MoveRotation(Quaternion.LookRotation(directionToPlayer));
-
+        Vector3 directionToTarget = new Vector3(targetPos.x - pos.x, 0, targetPos.z - pos.z).normalized;
+        Vector3 movement = directionToTarget * (speed * 10);
         float distOnXZ = Vector3.Distance(new Vector3(pos.x, 0, pos.z), new Vector3(targetPos.x, 0, targetPos.z));
-        
-        // Moving when not in stopping distance of the player
-        if (distOnXZ > stopDistance + stopDistance/2) rb.AddForce(movement);
+
+        // Rotate to face the player
+        rb.MoveRotation(Quaternion.LookRotation(directionToTarget));
+
+        // Moving when not in stopping distance of the target
+        if (distOnXZ > stopDistance + stopDistance / 2) rb.AddForce(movement);
         // Slow down enemy when in stopping distance
         else if (rb.velocity.magnitude > 0.5) rb.AddForce(-rb.velocity * 2);
         // Speed limit
         if (rb.velocity.magnitude > speed) rb.AddForce(-movement);
+        // Move away from the target when too close
         if (distOnXZ < 2)
         {
-            rb.AddForce(-movement/3);
+            rb.AddForce(-movement / 3);
             if (rb.velocity.magnitude > speed) rb.AddForce(movement);
         }
-        
+
         // State change check
         RaycastHit hit;
         Physics.Raycast(target.transform.position, Vector3.down, out hit, 100, terrainLayer);
         RaycastHit enemyHit;
         Physics.Raycast(transform.position, Vector3.down, out enemyHit, 100, terrainLayer);
-        if ((path.corners.Length > 2 || !LOSToTarget) && hit.point.y + 0.1 >= enemyHit.point.y && grounded && stateSwitchTimer < 0)
+        if ((path.corners.Length > 2 || !LOSToTarget) && hit.point.y + 0.1 >= enemyHit.point.y && grounded)
         {
-            stateSwitchTimer = 1;
-            
             state = State.navmesh;
         }
+    }
+
+    private void Attack()
+    {
+
     }
 
     private void Grounded()
     {
         Vector3 s = transform.localScale;
-        if (Physics.OverlapBox(transform.position + Vector3.down * s.y/2, new Vector3(s.x * 0.3f, s.y * 1, s.z * 0.3f), Quaternion.identity, terrainLayer).Length > 0)
+        if (Physics.OverlapBox(transform.position + Vector3.down * s.y / 2, new Vector3(s.x * 0.3f, s.y * 1, s.z * 0.3f), Quaternion.identity, terrainLayer).Length > 0)
             grounded = true;
         else grounded = false;
     }
