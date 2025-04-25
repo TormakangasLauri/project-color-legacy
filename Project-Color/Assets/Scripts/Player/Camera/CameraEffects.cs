8d6cd5f1d0178e486fdddc1f6b89f2e6fde2a4d9
Miko <miko.seppanen06@gmail.com>
2025-04-25 22:27:44 +0300

    Camera and UI testing

---
+++ Project-Color/Assets/Scripts/Player/Camera/CameraEffects.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffects : MonoBehaviour
+{
+    ShaderEffect_CorruptedVram[] shaderEffects;
+    public float shift = 0;
+
+    public float shiftMin = 0.5f;
+    public float shiftMax = 2;
+    public float minTime = 0.01f;
+    public float maxTime = 0.1f;
+
+    float timer = 0;
+    float timer1 = 0;
+    float timer2 = 0;
+
+    private void OnValidate()
+    {
+        shaderEffects = GetComponents<ShaderEffect_CorruptedVram>();
+        SetEffect();
+    }
+
+    void Start()
+    {
+        shaderEffects = GetComponents<ShaderEffect_CorruptedVram>();
+    }
+
+    void Update()
+    {
+        if (GameController.paused)
+        {
+            EffecsEnabled(true);
+            timer1 -= Time.unscaledDeltaTime;
+            timer2 -= Time.unscaledDeltaTime;
+            System.Random rand = new System.Random();
+            if (timer1 < 0)
+            {
+                SetEffect1Random();
+                timer1 = (float)rand.NextDouble()/3;
+            }
+            if (timer2 < 0)
+            {
+                SetEffect2Random();
+                timer2 = (float)rand.NextDouble()/3;
+            }
+        }
+        else
+        {
+            EffecsEnabled(false);
+            timer1 = 0;
+        }
+    }
+
+    void SetEffect()
+    {
+        if (shift == 0)
+        {
+            shaderEffects[0].enabled = false;
+            shaderEffects[1].enabled = false;
+        }
+        else
+        {
+            shaderEffects[0].enabled = true;
+            shaderEffects[1].enabled = true;
+            shaderEffects[0].shift = shift;
+            shaderEffects[1].shift = -shift;
+        }
+    }
+    
+    void SetEffect1Random()
+    {
+        float _shift = Random.Range(shiftMin, shiftMax);
+        shaderEffects[0].shift = _shift;
+    }
+    void SetEffect2Random()
+    {
+        float _shift = Random.Range(shiftMin, shiftMax);
+        shaderEffects[1].shift = -_shift;
+    }
+
+    private void EffecsEnabled(bool enabled)
+    {
+        shaderEffects[0].enabled = enabled;
+        shaderEffects[1].enabled = enabled;
+    }
+}
